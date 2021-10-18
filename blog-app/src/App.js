01f3c656c01e3e51eb8ec6fadc0bf94ebbeb8862
smweldes <weldesiem@gmail.com>
2021-10-18 01:09:25 -0700

    add header

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Home from './component/Home';
 import TopBar from './component/TopBar';
 
 function App() {
   return (
     <div>
       <TopBar/>
+      <Home/>
       
     </div>
   );
---
+++ blog-app/src/component/Home.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import './main.css';
+
+
+function Home() {
+    return (
+        <div className="h">
+            <Header/>
+        </div>
+    )
+}
+
+export default Home
