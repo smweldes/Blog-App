ebb7c9e0fa37f308299d25b5fb8085b75da40e1d
smweldes <weldesiem@gmail.com>
2021-10-18 16:14:05 -0700

    add sidebar

--- blog-app/src/component/Home.js
+++ blog-app/src/component/Home.js
@@ -1,11 +1,13 @@
 import Header from './Header';
 import './main.css';
+import Sidebar from './Sidebar';
 
 
 function Home() {
     return (
         <div className="h">
             <Header/>
+            <Sidebar/>
         </div>
     )
 }
