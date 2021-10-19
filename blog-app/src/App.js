e42f84d8ebd25a0b83d86500c03b0ded61e26d21
smweldes <weldesiem@gmail.com>
2021-10-19 02:51:54 -0700

    add write

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './component/Home';
 import TopBar from './component/TopBar';
+import Write from './component/Write';
 
 function App() {
   return (
     <div>
       <TopBar/>
-      <Home/>
+      {/* <Home/> */}
+      <Write/>
       
     </div>
   );
